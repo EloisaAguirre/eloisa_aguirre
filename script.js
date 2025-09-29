32ddff3b582dd11ec3e408db78e08e07d8e60383
EloisaAguirre <aguirre.elo@outlook.com>
2025-09-29 12:45:57 -0700

    Switch to Blaka Ink and Pixelify Sans fonts

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ button.addEventListener("click", function() {
   if (message.style.display === "none") {
     message.style.display = "block";
     button.textContent = "🫣 Hide the magic";
-    document.body.style.backgroundColor = "#ffe4e1";
+    document.body.style.backgroundColor = "#e6cc99ff";
   } else {
     message.style.display = "none";
     button.textContent = "✨ Reveal the magic!";
-    document.body.style.backgroundColor = "white";
+    document.body.style.backgroundColor = "#c5c177ff";
   }
 });
 
